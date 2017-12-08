c6f0dbac4659696006b788446e45345c6628c742
Sasha <sashkeetok_7@mail.ru>
2017-12-08 13:25:33 +0900

    spisok

--- listdir.cpp
+++ listdir.cpp
@@ -1,20 +1,33 @@
-#include "windows.h"
+#include <windows.h>
+#include <stdio.h>
 #pragma warning( disable : 4996)
 
 static int count;
 
 
 
-int _tmain(int argc, wchar_t* argv[]) {
+int main() {
     	wchar_t s[512];              	// текущая папка
     	GetCurrentDirectory(512, s);	// получить текущую папку
-    	wprintf(TEXT("Starting in: %s\n"), s);
+    	wprintf(L"Starting in: %s\n", s);
 
-    	count = 0;                  	// обнулить счетчик файлов    	
+    	count = 0;   // обнулить счетчик файлов    
+		HANDLE hFind;                   	// номер поиска
+		WIN32_FIND_DATA res;				// результат поиска
+		hFind = FindFirstFile(L"*",&res);
+		
+		do {
+    		count++; // некоторые файлы не считаются??
+    		wprintf(L"%s\n",res.cFileName);
+
+    		// ...
+    		// здесь будет обход в глубину
+		} while (FindNextFile(hFind, &res) != 0);
+		FindClose(hFind);
 
         //dfs();                     	// запустить обход в глубину
    	 
-    	wprintf(TEXT("File count = %d\n"), count);
+    	wprintf(L"File count = %d\n", count);
     	return 0;
 }
 
